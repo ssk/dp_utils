48e0875c325f2e122972d09bcec480aca6fea92f
Sam Kong <sam.s.kong@gmail.com>
2013-03-03 14:46:02 -0800

    sam

--- lib/dp_utils/railtie.rb
+++ lib/dp_utils/railtie.rb
@@ -3,6 +3,7 @@ module DpUtils
   class Railtie < Rails::Railtie
     rake_tasks do
       load File.expand_path('../dp_db.rake', __FILE__)
+      load File.expand_path('../dp.rake', __FILE__)
       load File.expand_path('../dp_generate.rake', __FILE__)
     end
   end
